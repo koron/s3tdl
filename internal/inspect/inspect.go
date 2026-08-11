fedced6534fbee90127c28b853e01a8a9d06c95d
MURAOKA Taro <koron.kaoriya@gmail.com>
2026-08-11 23:51:47 +0900

    Output the catalog information, anyway

--- internal/inspect/inspect.go
+++ internal/inspect/inspect.go
@@ -4,6 +4,8 @@ package inspect
 import (
 	"context"
 	"errors"
+	"fmt"
+	"log"
 
 	"github.com/koron-go/subcmd"
 	"github.com/koron/s3tdl/internal/common"
@@ -32,8 +34,49 @@ func inspectCatalog(ctx context.Context, arn string) error {
 	if err != nil {
 		return err
 	}
-	// TODO:
-	println("Hello inspect")
-	_ = cat
+
+	namespaces, err := cat.ListNamespaces(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	for _, ns := range namespaces {
+		fmt.Printf("Namespace: %s\n", common.ID2Str(ns))
+		props, err := cat.LoadNamespaceProperties(ctx, ns)
+		if err != nil {
+			log.Printf("failed to LoadNamespaceProperties: %s", err)
+		}
+		fmt.Printf("  Properties (%d)\n", len(props))
+		for k, v := range props {
+			fmt.Printf("    %s: s\n", k, v)
+		}
+
+		for tableID, err := range cat.ListTables(ctx, ns) {
+			if err != nil {
+				log.Printf("failed to ListTables: %s", err)
+				break
+			}
+			fmt.Printf("Table: %s\n", common.ID2Str(tableID))
+			table, err := cat.LoadTable(ctx, tableID)
+			if err != nil {
+				log.Printf("failed to LoadTable: %s", err)
+				continue
+			}
+
+			snapshot := table.CurrentSnapshot()
+			fmt.Printf("  Current Snapshot: %d\n", snapshot.SnapshotID)
+			fmt.Printf("    %+v\n", *snapshot)
+
+			schema := table.Schema()
+			fmt.Println("  Schema:")
+			for _, f := range schema.Fields() {
+				fmt.Printf("    %s\n", f.String())
+			}
+
+			partition := table.Spec()
+			fmt.Printf("  Partition Spec: %+v\n", partition)
+		}
+	}
+
 	return nil
 }
